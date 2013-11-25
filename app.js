a80c7b27e9d0122d70c2d0d55ce83334aa63b6f2
agrt56 <andreas.gaertner@mni.thm.de>
2013-11-25 14:07:33 +0100

    Added GPLv3 notice to source files.

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-/*
-    This file is generated and updated by Sencha Cmd. You can edit this file as
-    needed for your application, but these edits will have to be merged by
-    Sencha Cmd when it performs code generation tasks such as generating new
-    models, controllers or views and when running "sencha app upgrade".
+/*--------------------------------------------------------------------------------+
+  - Dateiname:		/app.js
+  - Beschreibung:	Einstieg für die Applikation. 
+  - Datum:			25.11.2013
+  - Autor(en):		Andreas Gärtner <andreas.gaertner@hotmail.com>
+  +--------------------------------------------------------------------------------+
+  This program is free software; you can redistribute it and/or modify it under 
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation; either version 3 of the License, or any later version.
+  +--------------------------------------------------------------------------------+
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License along with 
+  this program; if not, see <http://www.gnu.org/licenses/>.
+  +--------------------------------------------------------------------------------+
+  Inoffizielle deutsche Übersetzung (http://www.gnu.de/documents/gpl.de.html):
 
-    Ideally changes to this file would be limited and most work would be done
-    in other places (such as Controllers). If Sencha Cmd cannot merge your
-    changes and its generated code, it will produce a "merge conflict" that you
-    will need to resolve manually.
+  Dieses Programm ist freie Software. Sie können es unter den Bedingungen der 
+  GNU General Public License, wie von der Free Software Foundation veröffentlicht,
+  weitergeben und/oder modifizieren, entweder gemäß Version 3 der Lizenz oder 
+  jeder späteren Version.
+  +--------------------------------------------------------------------------------+
+  Die Veröffentlichung dieses Programms erfolgt in der Hoffnung, daß es Ihnen von
+  Nutzen sein wird, aber OHNE IRGENDEINE GARANTIE, sogar ohne die implizite Garantie
+  der MARKTREIFE oder der VERWENDBARKEIT FÜR EINEN BESTIMMTEN ZWECK. Details finden
+  Sie in der GNU General Public License.
+  Sie sollten ein Exemplar der GNU General Public License zusammen mit diesem Programm
+  erhalten haben. Falls nicht, siehe <http://www.gnu.org/licenses/>.
+  +--------------------------------------------------------------------------------+
 */
 
 Ext.application({
