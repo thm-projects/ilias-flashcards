f76a4d8c8967baa6fe0d3c8eb81a86a5a358f1f0
agrt56 <andreas.gaertner@mni.thm.de>
2013-12-04 01:30:18 +0100

    Added 'AboutPanel'.

---
+++ app/view/about/AboutPanel.js
@@ -0,0 +1,68 @@
+/*--------------------------------------------------------------------------------+
+  - Dateiname:		app/view/about/AboutPanel.js
+  - Beschreibung:	Hilfeseite der Applikation. 
+  - Datum:			25.11.2013
+  - Autor(en):		Andreas Gärtner <andreas.gaertner@hotmail.com>
+  +--------------------------------------------------------------------------------+
+  This program is free software; you can redistribute it and/or modify it under 
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation; either version 3 of the License, or any later version.
+  +--------------------------------------------------------------------------------+
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License along with 
+  this program; if not, see <http://www.gnu.org/licenses/>.
+  +--------------------------------------------------------------------------------+
+  Inoffizielle deutsche Übersetzung (http://www.gnu.de/documents/gpl.de.html):
+
+  Dieses Programm ist freie Software. Sie können es unter den Bedingungen der 
+  GNU General Public License, wie von der Free Software Foundation veröffentlicht,
+  weitergeben und/oder modifizieren, entweder gemäß Version 3 der Lizenz oder 
+  jeder späteren Version.
+  +--------------------------------------------------------------------------------+
+  Die Veröffentlichung dieses Programms erfolgt in der Hoffnung, daß es Ihnen von
+  Nutzen sein wird, aber OHNE IRGENDEINE GARANTIE, sogar ohne die implizite Garantie
+  der MARKTREIFE oder der VERWENDBARKEIT FÜR EINEN BESTIMMTEN ZWECK. Details finden
+  Sie in der GNU General Public License.
+  Sie sollten ein Exemplar der GNU General Public License zusammen mit diesem Programm
+  erhalten haben. Falls nicht, siehe <http://www.gnu.org/licenses/>.
+  +--------------------------------------------------------------------------------+
+*/
+
+Ext.define('LernApp.view.about.AboutPanel', {
+    extend: 'Ext.Panel',
+    xtype: 'aboutPanel',
+
+    requires: [
+    ],
+    
+    config: {
+        title: Messages.HELP,
+        iconCls: 'info'
+    },
+    
+    initialize: function() {
+        this.callParent(arguments);
+        
+        this.backButton = Ext.create('Ext.Button', {
+            text: Messages.BACK,
+            align: 'left', 
+            ui: 'back',
+            handler : function(button) {
+                LernApp.app.main.tabPanel.animateActiveItem(0, {
+                    type: 'slide', 
+                    direction: 'right'
+                });
+            }
+        });
+        
+        this.titleBar = Ext.create('Ext.TitleBar', {
+            docked: 'top',
+            title: 'Hilfe',
+            items: [this.backButton]
+        });
+        
+        this.add([this.titleBar]);
+    }
+});
